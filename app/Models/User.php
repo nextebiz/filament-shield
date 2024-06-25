5520819a6568042e758d62e1044ed25406dbe956
nextebiz <nextebiz.com@gmail.com>
2024-06-26 01:31:16 +0500

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use BezhanSalleh\FilamentShield\Traits\HasPanelShield;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable implements FilamentUser
+{
+    use HasFactory, Notifiable, HasRoles, HasPanelShield;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // dd(auth()->user()->roles);
+        if (auth()->user()->hasRole('super_admin')) {
+            return true;
+        };
+        if (auth()->user()->hasRole('panel_user')) {
+            return true;
+        };
+        if (auth()->user()->hasRole('category_manager')) {
+            return true;
+        };
+
+        return false;
+    }
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
